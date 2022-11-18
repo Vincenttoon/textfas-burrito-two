b91b33242844efd23d0af04948f1b3b3d02abdcd
Vincent Toon <vincenttoon22@gmail.com>
2022-11-18 00:04:17 -0600

    img not responding but saving when downloaded??

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -50,10 +50,10 @@ module.exports = () => {
           test: /\.css$/i,
           use: ["style-loader", "css-loader"],
         },
-        // {
-        //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        //   type: "asset/resource",
-        // },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/icons",
+        },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
